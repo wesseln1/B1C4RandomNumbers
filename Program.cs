d956546d2a92b55df976f7eaa48507bb0d977e0a
unknown <nwessel429@gmail.com>
2019-11-15 13:37:00 -0600

    added if statement to check  numbers

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace randomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int>
+            {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+            };
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+                if (i == numbers[i])
+                {
+                    Console.WriteLine($"the number is {numbers[i]}, and is contained in the lsit.");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is not contained in the list.");
+                }
+            }
+        }
+    }
+}
